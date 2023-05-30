6705662025c11bf7fb4f203b733fe091f33b23bf
inkma <115982948+tomaslmz@users.noreply.github.com>
2023-05-30 14:27:54 -0300

    realização da aula 14

--- aulas/aula06.07.08.09.10/banco.sql
+++ aulas/aula06.07.08.09.10/banco.sql
@@ -1,8 +1,14 @@
+CREATE DATABASE sistemadecadastro;
+
+USE sistemadecadastro;
+
 CREATE TABLE usuarios(
-	id INT PRIMARY KEY,
+	id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(30) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     idade INT NOT NULL
 );
 
-DESC usuarios;
+INSERT INTO usuarios (nome, email, idade) VALUES ("Tomás", "tomas153lm@gmail.com", 18);
+
+SELECT * FROM usuarios;
