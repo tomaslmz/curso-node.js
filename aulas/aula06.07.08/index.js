64c0edc304ba99df007d72ddb714a788a44169ff
inkma <115982948+tomaslmz@users.noreply.github.com>
2023-05-30 13:42:47 -0300

    realização da aula 8

--- aulas/aula06.07.08/index.js
+++ aulas/aula06.07.08/index.js
@@ -12,6 +12,11 @@ app.get("/sobre", (req, res) => {
 app.get("/blog", (req, res) => {
     res.send("Bem-vindo ao meu blog!");
 })
+
+app.get('/ola/:cargo/:nome', (req, res) => {
+    res.send("<h1>Nome: " + req.params.nome + "</h1>" + "<h1>Cargo: " + req.params.cargo + "</h1>");//Só é possível usar o send apenas uma vez
+})
+
 //localhost:8081
 app.listen(8081, () => {//Esse comando tem que ser sempre o último.
     console.log("servidor rodando em localhost:8081");
