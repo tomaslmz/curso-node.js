c685cc226da54ccd1ea875654872eb9c28015a46
inkma <115982948+tomaslmz@users.noreply.github.com>
2023-05-30 13:47:10 -0300

    finalização da aula 9

--- aulas/aula06.07.08.09/index.js
+++ aulas/aula06.07.08.09/index.js
@@ -16,7 +16,7 @@ app.get("/blog", (req, res) => {
 app.get('/ola/:cargo/:nome', (req, res) => {
     res.send("<h1>Nome: " + req.params.nome + "</h1>" + "<h1>Cargo: " + req.params.cargo + "</h1>");//Só é possível usar o send apenas uma vez
 })
-
+//Aula 09: instalação do nodemon
 //localhost:8081
 app.listen(8081, () => {//Esse comando tem que ser sempre o último.
     console.log("servidor rodando em localhost:8081");
