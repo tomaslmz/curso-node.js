75fb2f948a780698f3cc0e41279dd70d9057a3b9
inkma <115982948+tomaslmz@users.noreply.github.com>
2023-05-30 15:26:25 -0300

    realização da aula 18

--- aulas/aula17/teste.js
+++ aulas/aula17/teste.js
@@ -4,8 +4,47 @@ const sequelize = new Sequelize('teste', 'root', 'root', {
     dialect: "mysql",
 });
 
-sequelize.authenticate().then(() => {
-    console.log("Conectado com sucesso!");
-}).catch((erro) => {
-    console.log("Falha ao se conectar: ERRO " + erro);
+const postagens = sequelize.define('postagens', {//Criação 
+    titulo: {
+        type: Sequelize.STRING
+    },
+    conteudo: {
+        type: Sequelize.TEXT
+    }
 });
+
+const usuarios = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
+
+postagens.create({
+    titulo: "Título de postagem",
+    conteudo: "lorem"
+})
+
+usuarios.create({
+    nome: "Tomás",
+    sobrenome: "Mendoza",
+    idade: 18,
+    email: "tomas153@gmail.com"
+})
+
+// postagens.sync({force: true});
+// usuarios.sync({force: true});
+
+// sequelize.authenticate().then(() => {
+//     console.log("Conectado com sucesso!");
+// }).catch((erro) => {
+//     console.log("Falha ao se conectar: ERRO " + erro);
+// });
