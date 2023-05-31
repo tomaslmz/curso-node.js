1da3e89e7c83a1fef857f1ed19a2afc00f95c16d
inkma <115982948+tomaslmz@users.noreply.github.com>
2023-05-31 15:50:23 -0300

    realização da aula 20

--- aulas/aula17/index.js
+++ aulas/aula17/index.js
@@ -8,7 +8,7 @@ const Sequelize = require('sequelize');
 
 
 //configuração do handlebars
-app.engine('handlebars', handlebars({defaultLayout: 'main'}));
+app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 //Conexão com o bando de dados MySQL
 const sequelize = new Sequelize('teste', 'root', 'root', {
@@ -16,6 +16,10 @@ const sequelize = new Sequelize('teste', 'root', 'root', {
     dialect: "mysql",
 });
 
+app.get('/cadastro', (req, res) => {
+    res.render('formulario');
+})
+
 app.listen(8081, () => {
     console.log("Servidor rodando");
-})
+});
